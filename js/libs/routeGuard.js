5f93c6914ef37753f7767b0b50510b1a1f72c001
martinbolsnes <martinbolsnes@icloud.com>
2021-11-29 15:28:32 +0100

    added routeguard

--- js/libs/routeGuard.js
+++ js/libs/routeGuard.js
@@ -1,5 +1,5 @@
-import { getUser } from "./localStorageHelper,js";
+import { getUser } from "./localStorageHelper.js";
 
 if (getUser("jwt") === null) {
-  window.location.href = "./login.html";
+  window.location.href = "login.html";
 }
