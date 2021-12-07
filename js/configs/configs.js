9a5f2f868bbbc13afde2093dd9fdf88b7b034b4a
martinbolsnes <martinbolsnes@icloud.com>
2021-12-07 10:17:17 +0100

    removed .reÃplace config

--- js/configs/configs.js
+++ js/configs/configs.js
@@ -3,8 +3,6 @@ export const BASE_URL = `https://semester-project2-api.herokuapp.com`;
 export const header = {
   headers: {
     "Content-Type": "application/json",
-    Authorization: `Bearer ${localStorage
-      .getItem("jwt")
-      .replace(/^"(.*)"$/, "$1")}`,
+    Authorization: `Bearer ${localStorage.getItem("jwt")}`,
   },
 };
