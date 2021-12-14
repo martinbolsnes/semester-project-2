c510b328e8b05ef9dec61dc3b15f95bc429c8111
martinbolsnes <martinbolsnes@icloud.com>
2021-12-14 13:02:17 +0100

    reduced some css

--- js/cart.js
+++ js/cart.js
@@ -16,8 +16,8 @@ cartObjects.forEach((element) => {
   document.querySelector(".checkoutBtn").classList.remove("disabled");
   cartItems.innerHTML += `
         <div class="card cartCard">
-        <img src="${element.image_url}" class="card-img-top cartCard-img" alt="${element.title}">
-        <div class="card-body cartCard-body">
+        <img src="${element.image_url}" class="card-img-top cartCard__img" alt="${element.title}">
+        <div class="card-body cartCard__body">
         <a href="details.html?id=${element.id}"><p class="card-text card-text-title">${element.title}</p></a>
         <button class="removeBtn" data-id="${element.id}" data-title="${element.title}" data-image_url="${element.image_url}" data-price="${element.price}">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#b65242" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x-circle"><circle cx="12" cy="12" r="10"></circle><line x1="15" y1="9" x2="9" y2="15"></line><line x1="9" y1="9" x2="15" y2="15"></line></svg></button>  
